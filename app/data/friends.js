67e365f7c63e098df3cf8214a1e26c0442d9f9ce
AshSeitz <ash.seitz94@gmail.com>
2018-12-19 13:01:25 -0500

    git commit -m "friends data array set up"

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//data array format 
+
+var friendsData = [
+{
+    "name": "John",
+    "photo":"",
+    "scores": [
+        3,
+        4,
+        5,
+        1,
+        1,
+        4,
+        2,
+        1,
+        3,
+        2
+    
+    ]
+}
+]
