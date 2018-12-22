75f01a6741739af6621e410730bf726a6dcd8832
AshSeitz <ash.seitz94@gmail.com>
2018-12-22 15:08:20 -0500

    git commit -m "fix api route/try to fix survey javascript"

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,13 @@ var friendsData = require("../data/friends");
 //Routing
 
 module.exports = function(app) {
-    app.get("/api/friends"), function(req, res) {
+    app.get("/api/friends", function(req, res) {
+        
         res.json(friendsData)
-    };
+    });
 
-   // app.post("/api/friends", function (req, res){})
+   app.post("/api/friends", function (req, res){
+       friendsData.push(req.body)
+       res.json(newUser)
+   })
 }
